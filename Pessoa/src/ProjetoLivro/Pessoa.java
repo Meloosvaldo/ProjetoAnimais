a4a2fdb5fc5a350dcf5c758633cd5d455695d7f0
Osvaldo Antunes <meloosvaldo@hotmail.com>
2022-08-22 11:34:42 -0300

    Adicionando o arquivo Pessoa

---
+++ Pessoa/src/ProjetoLivro/Pessoa.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProjetoLivro;
+
+public class Pessoa {
+    //atributos
+    private String nome, sexo;
+    private int idade;
+    
+    //Métodos
+    public void fazerAniver(){
+        this.idade++;
+    }
+      //Construtor
+    public Pessoa(String nome, int idade,String sexo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+}
